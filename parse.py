11a69ae491263de800a502b5de3969a3223f4524
just_toch <zirech@gmail.com>
2026-01-20 02:53:43 +0300

    first commit

---
+++ parse.py
@@ -0,0 +1,303 @@
+from datetime import timedelta
+import requests
+import sqlite3
+import time
+import json
+import random
+from bs4 import BeautifulSoup
+from howlongtobeatpy import HowLongToBeat
+
+# ================== НАСТРОЙКИ ==================
+with open('steam_api.json', 'r', encoding='utf-8') as f:
+    STEAM_API_KEY = json.load(f)
+MIN_APP_TIME = 3.0
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0",
+    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7"
+}
+
+AGE_COOKIES = {
+    "birthtime": "568022401",
+    "lastagecheckage": "1-0-1990"
+}
+
+hltb = HowLongToBeat()
+
+# ================== БАЗЫ ==================
+
+games_db = sqlite3.connect("games.db")
+nongames_db = sqlite3.connect("nongames.db")
+
+games_cur = games_db.cursor()
+nongames_cur = nongames_db.cursor()
+
+games_cur.execute("""
+CREATE TABLE IF NOT EXISTS games (
+    appid INTEGER PRIMARY KEY,
+    name TEXT,
+    price_usd REAL,
+    short_description TEXT,
+    header_image TEXT,
+    developers TEXT,
+    publishers TEXT,
+    release_date TEXT,
+    supported_languages TEXT,
+    total_reviews INTEGER,
+    positive_reviews INTEGER,
+    negative_reviews INTEGER,
+    review_score TEXT,
+    hltb_main REAL,
+    hltb_extra REAL,
+    hltb_completion REAL,
+    hltb_id INTEGER
+)
+""")
+
+games_cur.execute("""
+CREATE TABLE IF NOT EXISTS game_categories (
+    steam_appid INTEGER,
+    category TEXT,
+    PRIMARY KEY (steam_appid, category),
+    FOREIGN KEY (steam_appid) REFERENCES games(appid)
+)
+""")
+
+games_cur.execute("""
+CREATE TABLE IF NOT EXISTS game_genres (
+    steam_appid INTEGER,
+    genre TEXT,
+    PRIMARY KEY (steam_appid, genre),
+    FOREIGN KEY (steam_appid) REFERENCES games(appid)
+)
+""")
+
+# Нормализованная таблица тегов
+games_cur.execute("""
+CREATE TABLE IF NOT EXISTS game_tags (
+    steam_appid INTEGER,
+    tag TEXT,
+    PRIMARY KEY (steam_appid, tag),
+    FOREIGN KEY (steam_appid) REFERENCES games(appid)
+)
+""")
+
+# Таблица для не-игр
+nongames_cur.execute("""
+CREATE TABLE IF NOT EXISTS items (
+    appid INTEGER PRIMARY KEY,
+    name TEXT,
+    type TEXT,
+    appdetails_json TEXT
+)
+""")
+
+games_db.commit()
+nongames_db.commit()
+
+# ================== STEAM ==================
+
+def get_appdetails(appid):
+    print(f"[{appid}] Парсим Steam API...")
+    url = "https://store.steampowered.com/api/appdetails"
+    params = {
+        "appids": appid,
+        "cc": "US",
+        "l": "ru"
+    }
+    r = requests.get(url, params=params, headers=HEADERS, timeout=10)
+    r.raise_for_status()
+    data = r.json()
+    return data.get(str(appid), {})
+
+def get_price_usd(data):
+    price = data.get("price_overview")
+    if not price:
+        return None
+    return price.get("final", 0) / 100
+
+def get_tags(appid):
+    print(f"[{appid}] Парсим теги...")
+    url = f"https://store.steampowered.com/app/{appid}?l=russian"
+    r = requests.get(url, headers=HEADERS, cookies=AGE_COOKIES, timeout=10)
+    if r.status_code != 200:
+        return []
+    soup = BeautifulSoup(r.text, "html.parser")
+    return [t.get_text(strip=True) for t in soup.select("a.app_tag")]
+
+def get_reviews_summary(appid):
+    print(f"[{appid}] Парсим отзывы Steam...")
+    url = f"https://store.steampowered.com/appreviews/{appid}"
+    params = {
+        "json": 1,
+        "language": "all",
+        "purchase_type": "all",
+        "filter": "all"
+    }
+    r = requests.get(url, params=params, headers=HEADERS, timeout=10)
+    r.raise_for_status()
+    data = r.json()
+    summary = data.get("query_summary", {})
+    return (
+        summary.get("total_reviews", 0),
+        summary.get("total_positive", 0),
+        summary.get("total_negative", 0),
+        summary.get("review_score_desc")
+    )
+
+# ================== HLTB ==================
+
+def get_hltb(game_name):
+    start_hltb = time.time()
+    print(f"[{game_name}] Парсим HLTB...")
+    try:
+        results = hltb.search(game_name)
+        if not results:
+            return None, None, None, None
+        r = results[0]
+        end_hltb = time.time()
+        print(end_hltb - start_hltb)
+        return r.main_story, r.main_extra, r.completionist, r.game_id
+    except:
+        end_hltb = time.time()
+        print(end_hltb - start_hltb)
+        return None, None, None, None
+
+
+# ================== ОБРАБОТКА ==================
+
+def process_app(appid):
+    start = time.time()
+    app = get_appdetails(appid)
+    data = app.get("data", {})
+    item_type = data.get("type")
+    name = data.get("name")
+
+    if not app.get("success"):
+        nongames_cur.execute(
+            "INSERT OR REPLACE INTO items VALUES (?,?,?,?)",
+            (appid, name, item_type, None)
+        )
+        nongames_db.commit()
+        return
+
+    if item_type != "game":
+        name = data.get("name")
+        nongames_cur.execute(
+            "INSERT OR REPLACE INTO items VALUES (?,?,?,?)",
+            (appid, name, item_type, json.dumps(data, ensure_ascii=False))
+        )
+        nongames_db.commit()
+        return
+
+    price = get_price_usd(data)
+    tags = get_tags(appid)
+    # Теги
+    games_cur.execute('DELETE FROM game_tags WHERE steam_appid=?', (appid,))
+    for tag in tags:
+        games_cur.execute('INSERT INTO game_tags (steam_appid, tag) VALUES (?,?)',
+                          (appid, tag))
+
+    total_reviews, positive_reviews, negative_reviews, review_score = get_reviews_summary(appid)
+    hltb_main, hltb_extra, hltb_completion, hltb_id = get_hltb(name)
+
+    supported_languages = data.get("supported_languages")
+
+    # Сохраняем основное
+    games_cur.execute("""
+    INSERT OR REPLACE INTO games
+    (appid, name, price_usd, short_description, header_image, developers, publishers, release_date,
+     supported_languages,
+     total_reviews, positive_reviews, negative_reviews, review_score,
+     hltb_main, hltb_extra, hltb_completion, hltb_id)
+    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+    """, (
+        appid,
+        name,
+        price,
+        data.get("short_description"),
+        data.get("header_image"),
+        ", ".join(data.get("developers", [])),
+        ", ".join(data.get("publishers", [])),
+        data.get("release_date", {}).get("date"),
+        supported_languages,
+        total_reviews,
+        positive_reviews,
+        negative_reviews,
+        review_score,
+        hltb_main,
+        hltb_extra,
+        hltb_completion,
+        hltb_id,
+        # json.dumps(data, ensure_ascii=False)
+    ))
+
+    # Категории
+    games_cur.execute('DELETE FROM game_categories WHERE steam_appid=?', (appid,))
+    for cat in data.get("categories", []):
+        games_cur.execute('INSERT INTO game_categories (steam_appid, category) VALUES (?,?)',
+                          (appid, cat.get("description")))
+
+    # Жанры
+    games_cur.execute('DELETE FROM game_genres WHERE steam_appid=?', (appid,))
+    for genre in data.get("genres", []):
+        games_cur.execute('INSERT INTO game_genres (steam_appid, genre) VALUES (?,?)',
+                          (appid, genre.get("description")))
+
+
+
+    games_db.commit()
+
+    elapsed = time.time() - start
+    if elapsed < MIN_APP_TIME:
+        time.sleep(MIN_APP_TIME - elapsed)
+
+# ================== ЗАПУСК ==================
+
+def fetch_test_appids(n=5):
+    test_ids = appids
+    return test_ids[:n]
+
+
+def format_eta(seconds):
+    if seconds < 0:
+        return "0s"
+    return str(timedelta(seconds=int(seconds)))
+
+processed_times = []
+start_all = None
+
+if __name__ == "__main__":
+    FILE = "steam_appids.json"
+    with open(FILE, 'r', encoding='utf-8') as f:
+        appids = json.load(f)
+    appids = [22490, 343300]
+    # appids = fetch_test_appids()
+    total = len(appids)
+
+    start_all = time.time()
+
+    for idx, appid in enumerate(appids, 1):
+        app_start = time.time()
+
+        try:
+            print(f"\n=== Обработка {idx}/{total} AppID ({idx/total*100:.1f}%) ===")
+            process_app(appid)
+            status = "Готово"
+        except Exception as e:
+            status = f"Ошибка: {e}"
+
+        elapsed = time.time() - app_start
+        processed_times.append(elapsed)
+
+        avg_time = sum(processed_times) / len(processed_times)
+        remaining = total - idx
+        eta_seconds = avg_time * remaining
+
+        print(f"[{appid}] {status}")
+        print(
+            f"Время: {elapsed:.2f}s | "
+            f"Среднее: {avg_time:.2f}s | "
+            f"Осталось: {remaining} | "
+            f"ETA: {format_eta(eta_seconds)}"
+        )
