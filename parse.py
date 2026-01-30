33266ece5bae6ca97e81c9bfa1f380d5062105a1
toch <zirech@gmail.com>
2026-01-30 17:59:00 +0300

    7

--- parse.py
+++ parse.py
@@ -10,8 +10,6 @@ from bs4 import BeautifulSoup
 from howlongtobeatpy import HowLongToBeat
 
 # ================== НАСТРОЙКИ ==================
-with open('steam_api.json', 'r', encoding='utf-8') as f:
-    STEAM_API_KEY = json.load(f)
 MIN_APP_TIME = 3.0
 MAX_RETRIES = 3
 SKIPPED_FILE = "skipped_appids.json"
@@ -64,14 +62,14 @@ CREATE TABLE IF NOT EXISTS games (
     price_usd REAL,
     short_description TEXT,
     header_image TEXT,
-    developers TEXT,
-    publishers TEXT,
-    release_date TEXT,
-    supported_languages TEXT,
+    release_year INTEGER,
+    release_month INTEGER,
+    release_day INTEGER,
     total_reviews INTEGER,
     positive_reviews INTEGER,
     negative_reviews INTEGER,
-    review_score TEXT,
+    review_percent INTEGER,
+    review_score INTEGER,
     hltb_main REAL,
     hltb_extra REAL,
     hltb_completion REAL,
@@ -80,30 +78,78 @@ CREATE TABLE IF NOT EXISTS games (
 """)
 
 games_cur.execute("""
-CREATE TABLE IF NOT EXISTS game_categories (
-    appid INTEGER,
-    category TEXT,
-    PRIMARY KEY (appid, category),
-    FOREIGN KEY (appid) REFERENCES games(appid)
+CREATE TABLE IF NOT EXISTS tags_dict (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE
 )
 """)
 
 games_cur.execute("""
-CREATE TABLE IF NOT EXISTS game_genres (
+CREATE TABLE IF NOT EXISTS tags_games (
     appid INTEGER,
-    genre TEXT,
-    PRIMARY KEY (appid, genre),
-    FOREIGN KEY (appid) REFERENCES games(appid)
+    tag_id INTEGER,
+    PRIMARY KEY (appid, tag_id)
 )
 """)
 
 games_cur.execute("""
-CREATE TABLE IF NOT EXISTS game_tags (
-    appid INTEGER,
-    tag TEXT,
-    PRIMARY KEY (appid, tag),
-    FOREIGN KEY (appid) REFERENCES games(appid)
+CREATE TABLE IF NOT EXISTS genres_dict (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE
 )
+""")
+
+games_cur.execute("""
+CREATE TABLE IF NOT EXISTS genres_games (
+    appid INTEGER,
+    genre_id INTEGER,
+    PRIMARY KEY (appid, genre_id)
+)
+""")
+
+games_cur.execute("""
+CREATE TABLE IF NOT EXISTS categories_dict (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE
+)
+""")
+
+games_cur.execute("""
+CREATE TABLE IF NOT EXISTS categories_games (
+    appid INTEGER,
+    category_id INTEGER,
+    PRIMARY KEY (appid, category_id)
+)
+""")
+
+games_cur.execute("""
+CREATE TABLE IF NOT EXISTS developers_dict (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE
+);
+""")
+
+games_cur.execute("""
+CREATE TABLE IF NOT EXISTS developers_games (
+    appid INTEGER,
+    developer_id INTEGER,
+    PRIMARY KEY (appid, developer_id)
+);
+""")
+
+games_cur.execute("""
+CREATE TABLE IF NOT EXISTS publishers_dict (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE
+);
+""")
+
+games_cur.execute("""
+CREATE TABLE IF NOT EXISTS publishers_games (
+    appid INTEGER,
+    publisher_id INTEGER,
+    PRIMARY KEY (appid, publisher_id)
+);
 """)
 
 games_cur.execute("""
@@ -116,6 +162,22 @@ CREATE TABLE IF NOT EXISTS parser_state (
 games_cur.execute("""
 INSERT OR IGNORE INTO parser_state (id, last_appid)
 VALUES (1, 0)
+""")
+
+games_cur.execute("""
+CREATE TABLE IF NOT EXISTS languages_dict (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE
+)
+""")
+
+games_cur.execute("""
+CREATE TABLE IF NOT EXISTS languages_games (
+    appid INTEGER,
+    language_id INTEGER,
+    full_audio INTEGER,
+    PRIMARY KEY (appid, language_id)
+)
 """)
 
 nongames_cur.execute("""
@@ -147,7 +209,6 @@ def get_appdetails(appid):
     return data.get(str(appid), {})
 
 def get_appdetails_ru(appid):
-    print(f"[{appid}]")
     url = "https://store.steampowered.com/api/appdetails"
     params = {
         "appids": appid,
@@ -191,12 +252,10 @@ def get_reviews_summary(appid):
         summary.get("total_reviews", 0),
         summary.get("total_positive", 0),
         summary.get("total_negative", 0),
-        summary.get("review_score_desc")
+        summary.get("review_score")
     )
 
 # ================== HLTB ==================
-
-
 
 def get_hltb(game_name):
     start_hltb = time.time()
@@ -268,10 +327,6 @@ def process_app(appid):
         label="Steam tags"
     )
 
-    for tag in tags:
-        games_cur.execute('INSERT INTO game_tags (appid, tag) VALUES (?,?)',
-                          (appid, tag))
-
     total_reviews, positive_reviews, negative_reviews, review_score = retry_call(
         get_reviews_summary,
         appid,
@@ -280,19 +335,19 @@ def process_app(appid):
     )
 
     if data.get("release_date", {}).get('coming_soon'):
-        release_date = 'Coming soon'
+        release_year, release_month, release_day = None, None, None
         hltb_main, hltb_extra, hltb_completion, hltb_id = None, None, None, None
     else:
-        release_date = convert_release_date(data_ru.get("release_date", {}).get("date"))
+        release_year, release_month, release_day = convert_release_date(data_ru.get("release_date", {}).get("date"))
         hltb_main, hltb_extra, hltb_completion, hltb_id = get_hltb(name)
 
-    supported_languages = data.get("supported_languages")
+    languages = parse_supported_languages(data.get("supported_languages"))
 
     games_cur.execute("""
     INSERT OR REPLACE INTO games
-    (appid, name, price_usd, short_description, header_image, developers, publishers, release_date,
-     supported_languages,
-     total_reviews, positive_reviews, negative_reviews, review_score,
+    (appid, name, price_usd, short_description, header_image,
+     release_year, release_month, release_day,
+     total_reviews, positive_reviews, negative_reviews, review_percent, review_score,
      hltb_main, hltb_extra, hltb_completion, hltb_id)
     VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
     """, (
@@ -301,44 +356,112 @@ def process_app(appid):
         price,
         data_ru.get("short_description"),
         data.get("header_image"),
-        ", ".join(data.get("developers", [])),
-        ", ".join(data.get("publishers", [])),
-        release_date,
-        supported_languages,
+        release_year,
+        release_month,
+        release_day,
         total_reviews,
         positive_reviews,
         negative_reviews,
+        int((positive_reviews / total_reviews) * 100),
         review_score,
         hltb_main,
         hltb_extra,
         hltb_completion,
-        hltb_id,
-        # json.dumps(data, ensure_ascii=False)
+        hltb_id
     ))
 
-    # Категории
+    for lang, has_audio in languages.items():
+        games_cur.execute(
+            "INSERT OR IGNORE INTO languages_dict (name) VALUES (?)",
+            (lang,)
+        )
+
+        games_cur.execute(
+            """
+            INSERT OR REPLACE INTO languages_games
+            (appid, language_id, full_audio)
+            VALUES (
+                ?,
+                (SELECT id FROM languages_dict WHERE name = ?),
+                ?
+            )
+            """,
+            (appid, lang, 1 if has_audio else 0)
+        )
+
     categories = {
         c["description"].strip()
         for c in data.get("categories", [])
         if c.get("description")
     }
 
-    games_cur.executemany(
-        'INSERT OR IGNORE INTO game_categories (appid, category) VALUES (?, ?)',
-        [(appid, cat) for cat in categories]
-    )
+    for cat in categories:
+        games_cur.execute(
+            "INSERT OR IGNORE INTO categories_dict (name) VALUES (?)",
+            (cat,)
+        )
+        games_cur.execute(
+            """
+            INSERT OR IGNORE INTO categories_games (appid, category_id)
+            VALUES (
+                ?,
+                (SELECT id FROM categories_dict WHERE name = ?)
+            )
+            """,
+            (appid, cat)
+        )
 
-    # Жанры
     genres = {
         g["description"].strip()
         for g in data.get("genres", [])
         if g.get("description")
     }
 
-    games_cur.executemany(
-        'INSERT OR IGNORE INTO game_genres (appid, genre) VALUES (?, ?)',
-        [(appid, genre) for genre in genres]
-    )
+    for genre in genres:
+        games_cur.execute(
+            "INSERT OR IGNORE INTO genres_dict (name) VALUES (?)",
+            (genre,)
+        )
+        games_cur.execute(
+            """
+            INSERT OR IGNORE INTO genres_games (appid, genre_id)
+            VALUES (
+                ?,
+                (SELECT id FROM genres_dict WHERE name = ?)
+            )
+            """,
+            (appid, genre)
+        )
+
+    for tag in tags:
+        games_cur.execute(
+            "INSERT OR IGNORE INTO tags_dict (name) VALUES (?)",
+            (tag,)
+        )
+        games_cur.execute(
+            """
+            INSERT OR IGNORE INTO tags_games (appid, tag_id)
+            VALUES (
+                ?,
+                (SELECT id FROM tags_dict WHERE name = ?)
+            )
+            """,
+            (appid, tag)
+        )
+
+    for dev in data.get("developers", []):
+        games_cur.execute("INSERT OR IGNORE INTO developers_dict (name) VALUES (?)", (dev,))
+        games_cur.execute("""
+            INSERT OR IGNORE INTO developers_games (appid, developer_id)
+            VALUES (?, (SELECT id FROM developers_dict WHERE name=?))
+        """, (appid, dev))
+
+    for pub in data.get("publishers", []):
+        games_cur.execute("INSERT OR IGNORE INTO publishers_dict (name) VALUES (?)", (pub,))
+        games_cur.execute("""
+            INSERT OR IGNORE INTO publishers_games (appid, publisher_id)
+            VALUES (?, (SELECT id FROM publishers_dict WHERE name=?))
+        """, (appid, pub))
 
     games_db.commit()
     set_last_processed_appid(appid)
@@ -378,28 +501,43 @@ def retry_call(func, *args, retries=MAX_RETRIES, delay=2, appid=None, label=""):
                 raise
 
 
-# ================== ЗАПУСК ==================
+# ================== УТИЛИТЫ ==================
 
 def random_test_appids(n=5):
     test_ids = appids
     random.shuffle(test_ids)
     return test_ids[:n]
 
-
 def format_eta(seconds):
     if seconds < 0:
         return "0s"
     return str(timedelta(seconds=int(seconds)))
 
-
-def convert_release_date(date_str: str) -> str:
+def convert_release_date(date_str: str) -> tuple[int, int, int]:
     date_str = date_str.lower().strip()
     date_str = re.sub(r'\s*г\.\s*$', '', date_str)
     parts = date_str.split()
     day, month_rus, year = parts
     month = RU_MONTHS.get(month_rus[:3])
-    return f"{year}-{month}-{int(day):02d}"
+    return int(year), int(month), int(day)
 
+def parse_supported_languages(raw: str):
+    if not raw:
+        return {}
+
+    raw = re.sub(r"<.*?>", "", raw)
+    raw = re.sub(r"\*?\s*languages with full audio support.*$", "", raw, flags=re.IGNORECASE)
+    parts = [p.strip() for p in raw.split(",") if p.strip()]
+
+    languages = {}
+    for part in parts:
+        if part.endswith("*"):
+            name = part[:-1].strip()
+            languages[name] = True
+        else:
+            languages[part] = False
+
+    return languages
 
 processed_times = []
 start_all = None
@@ -417,7 +555,7 @@ if __name__ == "__main__":
         appids = [a for a in appids if a > last_appid]
 
     # appids = [1599660]
-    appids = random_test_appids(150)
+    # appids = random_test_appids(5)
     total = len(appids)
 
     start_all = time.time()
